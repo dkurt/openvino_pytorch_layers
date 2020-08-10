b42f3a1e495c3d67b0aa574707057fab0b57f326
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-08-10 11:24:32 +0300

    Add MaxUnpool guide

---
+++ mo_extensions/front/onnx/max_unpool.py
@@ -0,0 +1,41 @@
+# mo_extensions/front/onnx/max_unpool.py
+import numpy as np
+
+from mo.front.common.replacement import FrontReplacementSubgraph
+from mo.graph.graph import Graph
+from mo_extensions.ops.MaxPoolGrad import MaxPoolGrad
+
+class MaxUnpool(FrontReplacementSubgraph):
+    enabled = True
+
+    def pattern(self):
+        return dict(
+            nodes=[
+                ('input', dict()),
+                ('max_pool0', dict(op='MaxPool')),
+                ('max_pool1', dict(op='MaxPool')),
+                ('slice', dict(op='Slice')),
+                ('sub', dict(op='Sub')),
+                ('unpool', dict(op='Unpooling')),
+            ],
+            edges=[
+                ('input', 'max_pool0'),
+                ('input', 'max_pool1'),
+                ('max_pool1', 'slice'),
+                ('max_pool0', 'sub', {'in': 0}),
+                ('slice', 'sub', {'in': 1}),
+                ('sub', 'unpool', {'in': 1}),
+            ])
+
+    @staticmethod
+    def replace_sub_graph(graph: Graph, match: dict):
+        max_pool_input = match['input']
+        max_pool = match['max_pool0']
+        unpool = match['unpool']
+        unpool_input = unpool.in_port(0).get_source().node
+
+        max_pool.out_port(1).disconnect()
+
+        # Inputs: [max_pool_input, max_pool_output, unpool_input]
+        res = MaxPoolGrad(graph, dict(name=unpool.name + '/fused')).create_node([max_pool_input, max_pool, unpool_input])
+        unpool.out_port(0).get_connection().set_source(res.out_port(0))
---
+++ unpool.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class Unpool2d(torch.autograd.Function):
+    @staticmethod
+    def symbolic(g, x, indices):
+        return g.op('Unpooling', x, indices)
+
+    def forward(self, x, indices):
+        return nn.MaxUnpool2d(2, stride=2)(x, indices)
