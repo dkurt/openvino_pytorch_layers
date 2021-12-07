79a2ece82cd8d2688172ab366d362026d53ae033
Anna Likholat <anna.likholat@intel.com>
2021-12-07 13:11:48 +0300

    fix

--- user_ie_extensions/fft_impl.cpp
+++ user_ie_extensions/fft_impl.cpp
@@ -17,6 +17,11 @@ using cvReleaseMatF = void(CvMat**);
 using cvDftF = void(const CvArr*, CvArr*, int, int);
 using cvScaleF = void(const CvArr*, CvArr*, double, double);
 
+using cvCloneMatF = CvMat*(const CvMat*);
+using cvCopyF = void(const CvArr*, const CvArr*, const CvArr*);
+using cvInitMatHeaderF = CvMat*(CvMat*, int, int, int, void*, int);
+using cvGetRawDataF = void(const CvArr*, uchar**, int* step, CvSize* roi_size);
+
 bool loadOpenCV() {
     static bool loaded = false;
     if (!loaded) {
@@ -103,36 +108,90 @@ InferenceEngine::StatusCode FFTImpl::init(InferenceEngine::LayerConfig &config, 
 }
 //! [cpu_implementation:init]
 
-static cv::Mat infEngineBlobToMat(const InferenceEngine::Blob::Ptr& blob)
-{
-    // NOTE: Inference Engine sizes are reversed.
-    std::vector<size_t> dims = blob->getTensorDesc().getDims();
-    std::vector<int> size(dims.begin(), dims.end());
-    auto precision = blob->getTensorDesc().getPrecision();
-    CV_Assert(precision == InferenceEngine::Precision::FP32);
-    return cv::Mat(size, CV_32F, (void*)blob->buffer());
-}
+static void fftshift(CvMat* src, int rows, int cols) {
+    static auto cvCloneMat = reinterpret_cast<cvCloneMatF*>(so->get_symbol("cvCloneMat"));
+    static auto cvCopy = reinterpret_cast<cvCopyF*>(so->get_symbol("cvCopy"));
+    static auto cvInitMatHeader = reinterpret_cast<cvInitMatHeaderF*>(so->get_symbol("cvInitMatHeader"));
+    static auto cvGetRawData = reinterpret_cast<cvGetRawDataF*>(so->get_symbol("cvGetRawData"));
+    static auto cvCreateMatHeader = reinterpret_cast<cvCreateMatHeaderF*>(so->get_symbol("cvCreateMatHeader"));
+    static auto cvSetData = reinterpret_cast<cvSetDataF*>(so->get_symbol("cvSetData"));
 
-static void fftshift(cv::Mat& src)
-{
-    const int h_2 = src.rows / 2;
-    const int w_2 = src.cols / 2;
 
     // tl | tr        br | bl
     // ---+---   ->   ---+---
     // bl | br        tr | tl
-    cv::Mat tl = src({0, 0, w_2, h_2});
-    cv::Mat tr = src({w_2, 0, w_2, h_2});
-    cv::Mat bl = src({0, h_2, w_2, h_2});
-    cv::Mat br = src({w_2, h_2, w_2, h_2});
 
-    cv::Mat tmp = tl.clone();
-    br.copyTo(tl);
-    tmp.copyTo(br);
+    int h = rows / 2;
+    int w = cols / 2;
 
-    tr.copyTo(tmp);
-    bl.copyTo(tr);
-    tmp.copyTo(bl);
+    // CvMat* tl, tr, bl, br;
+
+   
+
+    float* src_data;
+    int src_step;
+    CvSize src_size;
+    cvGetRawData(src, (uchar**)&src_data, &src_step, &src_size);
+
+    std::cout << "INPUT step size" << std::endl;
+    std::cout << src_step / sizeof(float) << std::endl;
+    std::cout << src_size.height << " " << src_size.width <<std::endl;
+    
+    // std::cout << "INPUT" << std::endl;
+
+    // 
+
+    // // for(int i = step1; i < size1.width * size1.height; i++) {
+    // //     std::cout << (float)data1[i*step1] << " ";
+    // //     std::cout << (float)data1[i*step1 + 1] << " ";
+    // //     std::cout << (float)data1[i*step1 + 2] << " ";
+    // //     std::cout << (float)data1[i*step1 + 3] << " ";
+    // // }
+    // // std::cout << std::endl;
+
+    // for(int y = 0; y < size1.height; y++, data1 += step1 ) {
+    //     for(int x = 0; x < size1.width; x++)
+    //         std::cout << data1[x] << std::endl;
+    //     std::cout << std::endl;
+    // }
+
+    std::cout << "h = " << h <<std::endl;
+    std::cout << "w = " << w <<std::endl;
+    src_step /= sizeof(src_data[0]);
+    CvMat* tl = new CvMat();
+    cvInitMatHeader(tl, h, w, CV_32FC2, src, src_step * sizeof(float));
+    CvMat* tr = new CvMat();
+    cvInitMatHeader(tr, h, w, CV_32FC2, src + (src_step / 2), src_step * sizeof(float));
+    CvMat* bl = new CvMat();
+    cvInitMatHeader(bl, h, w, CV_32FC2, src + (src_step), src_step * sizeof(float));
+    CvMat* br = new CvMat();
+    cvInitMatHeader(br, h, w, CV_32FC2, src + (src_step * 3 / 2), src_step * sizeof(float));
+
+    int len = h * w;
+    int* mask = new int[len];
+    memset(mask, 0, len);
+
+    CvMat* maskMat = cvCreateMatHeader(h, w, CV_8U);
+    cvSetData(maskMat, reinterpret_cast<void*>(mask), src_step * sizeof(int));
+
+    CvMat* tmp = cvCloneMat(tl);
+    cvCopy(br, tl, maskMat);
+    cvCopy(tmp, br, maskMat);
+
+    cvCopy(tr, tmp, maskMat);
+    cvCopy(bl, tr, maskMat);
+    cvCopy(tmp, bl, maskMat);
+
+    std::cout << "END FFTSHIFT" << std::endl;
+
+    // cv::Mat tmp = tl.clone();
+    // br.copyTo(tl);
+    // tmp.copyTo(br);
+
+    // tr.copyTo(tmp);
+    // bl.copyTo(tr);
+    // tmp.copyTo(bl);
+
 }
 
 //! [cpu_implementation:execute]
@@ -150,6 +209,7 @@ InferenceEngine::StatusCode FFTImpl::execute(std::vector<InferenceEngine::Blob::
     std::vector<size_t> dims = inputs[0]->getTensorDesc().getDims();
 
     if (dims.size() == 5) {
+        
         const int batch = dims[0];
         const int channels = dims[1];
         int rows = dims[2];
@@ -160,6 +220,9 @@ InferenceEngine::StatusCode FFTImpl::execute(std::vector<InferenceEngine::Blob::
             CvMat* out = cvCreateMatHeader(rows, cols, CV_32FC2);
             cvSetData(inp, reinterpret_cast<void*>(inpData + d * planeSize), cols * 2 * sizeof(float));
             cvSetData(out, reinterpret_cast<void*>(outData + d * planeSize), cols * 2 * sizeof(float));
+
+            fftshift(inp, rows, cols);
+
             if (inverse)
                 cvDFT(inp, out, CV_DXT_INVERSE, 0);
             else
@@ -194,238 +257,4 @@ InferenceEngine::StatusCode FFTImpl::execute(std::vector<InferenceEngine::Blob::
     }
     return InferenceEngine::OK;
 }
-
-// InferenceEngine::StatusCode FFTImpl::execute(std::vector<InferenceEngine::Blob::Ptr> &inputs,
-//                                              std::vector<InferenceEngine::Blob::Ptr> &outputs,
-//                                              InferenceEngine::ResponseDesc *resp) noexcept {
-//     static auto cvSetData = reinterpret_cast<cvSetDataF*>(so->get_symbol("cvSetData"));
-//     static auto cvCreateMatHeader = reinterpret_cast<cvCreateMatHeaderF*>(so->get_symbol("cvCreateMatHeader"));
-//     static auto cvDFT = reinterpret_cast<cvDftF*>(so->get_symbol("cvDFT"));
-//     static auto cvScale = reinterpret_cast<cvScaleF*>(so->get_symbol("cvConvertScale"));
-//     static auto cvReleaseMat = reinterpret_cast<cvReleaseMatF*>(so->get_symbol("cvReleaseMat"));
-
-//     float* inpData = inputs[0]->buffer();
-//     float* outData = outputs[0]->buffer();
-//     std::vector<size_t> dims = inputs[0]->getTensorDesc().getDims();
-
-//     if (dims.size() == 5) {
-//         const int batch = dims[0];
-//         const int channels = dims[1];
-//         int rows = dims[2];
-//         int cols = dims[3];
-//         int planeSize = rows * cols * 2;  // 2 is last dimension size
-//         InferenceEngine::parallel_for(batch * channels, [&](size_t d) {
-//             CvMat* inp = cvCreateMatHeader(rows, cols, CV_32FC2);
-//             CvMat* out = cvCreateMatHeader(rows, cols, CV_32FC2);
-//             cvSetData(inp, reinterpret_cast<void*>(inpData + d * planeSize), cols * 2 * sizeof(float));
-//             cvSetData(out, reinterpret_cast<void*>(outData + d * planeSize), cols * 2 * sizeof(float));
-//             if (inverse)
-//                 cvDFT(inp, out, CV_DXT_INVERSE, 0);
-//             else
-//                 cvDFT(inp, out, CV_DXT_FORWARD, 0);
-//             cvScale(out, out, 1.0 / sqrtf(cols * rows), 0);
-
-//             cvReleaseMat(&inp);
-//             cvReleaseMat(&out);
-//         });
-//     } else {
-//         int rows, cols;
-//         if (dims.size() == 4) {
-//             rows = dims[0] * dims[1];
-//             cols = dims[2];
-//         } else if (dims.size() == 3) {
-//             rows = dims[0];
-//             cols = dims[1];
-//         }
-//         CvMat* inp = cvCreateMatHeader(rows, cols, CV_32FC2);
-//         CvMat* out = cvCreateMatHeader(rows, cols, CV_32FC2);
-//         cvSetData(inp, reinterpret_cast<void*>(inpData), cols * 2 * sizeof(float));
-//         cvSetData(out, reinterpret_cast<void*>(outData), cols * 2 * sizeof(float));
-
-//         if (inverse)
-//             cvDFT(inp, out, CV_DXT_INVERSE | CV_DXT_ROWS, 0);
-//         else
-//             cvDFT(inp, out, CV_DXT_FORWARD | CV_DXT_ROWS, 0);
-//         cvScale(out, out, 1.0 / sqrtf(cols), 0);
-
-//         cvReleaseMat(&inp);
-//         cvReleaseMat(&out);
-//     }
-//     return InferenceEngine::OK;
-// }
 //! [cpu_implementation:execute]
-
-
-/*
- Copyright (c) 2018-2019 Intel Corporation
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-// ===============================================================================
-// Generated file for Inference Engine extension for CPU plugin
-//
-// IMPLEMENT YOUR KERNEL HERE.
-//
-// You need to edit this file in order to:
-//  1. initialize parameters (in constructor)
-//  2. implement inference logic (in execute() method)
-//
-// Refer to the section "Adding Your Own Kernels to the Inference Engine" in
-// OpenVINO* documentation (either online or offline in
-// <INSTALL_DIR>/deployment_tools/documentation/docs/index.html an then navigate
-// to the corresponding section).
-// ===============================================================================
-
-// #include "ext_list.hpp"
-// #include "ext_base.hpp"
-// #include <cmath>
-// #include <vector>
-// #include <string>
-// #include <algorithm>
-
-// #include <opencv2/opencv.hpp>
-// #include <inference_engine.hpp>
-
-// namespace InferenceEngine {
-// namespace Extensions {
-// namespace Cpu {
-
-
-
-
-
-// class FFT2DImpl: public ExtLayerBase {
-// public:
-//     explicit FFT2DImpl(const CNNLayer* layer) {
-//         inverse = layer->type == "IFFT2D";
-//         addConfig(layer, { { ConfLayout::PLN, false, 0 } }, { { ConfLayout::PLN, false, 0 } });
-//     }
-
-//     StatusCode execute(std::vector<Blob::Ptr>& inputs, std::vector<Blob::Ptr>& outputs,
-//                        ResponseDesc *resp) noexcept override {
-
-//         cv::Mat inp = infEngineBlobToMat(inputs[0]);
-//         cv::Mat out = infEngineBlobToMat(outputs[0]);
-
-//         const int batchSize = inp.size[0];
-//         for (int i = 0; i < batchSize; ++i)
-//         {
-//             cv::Mat shifted_inp;
-//             std::vector<cv::Mat> complex = {
-//                 cv::Mat(inp.size[2], inp.size[3], CV_32F, inp.ptr<float>(i, 0)),
-//                 cv::Mat(inp.size[2], inp.size[3], CV_32F, inp.ptr<float>(i, 1))
-//             };
-//             cv::merge(complex, shifted_inp);
-
-//             fftshift(shifted_inp);
-
-//             cv::Mat interleaved_out;
-//             if (!inverse)
-//                 cv::dft(shifted_inp, interleaved_out);
-//             else
-//                 cv::idft(shifted_inp, interleaved_out, cv::DFT_SCALE);
-
-//             fftshift(interleaved_out);
-
-//             complex = {
-//                 cv::Mat(out.size[2], out.size[3], CV_32F, out.ptr<float>(i, 0)),
-//                 cv::Mat(out.size[2], out.size[3], CV_32F, out.ptr<float>(i, 1))
-//             };
-//             cv::split(interleaved_out, complex);
-//         }
-//         return OK;
-//     }
-// private:
-//     bool inverse;
-// };
-
-// REG_FACTORY_FOR(ImplFactory<FFT2DImpl>, FFT2D);
-// REG_FACTORY_FOR(ImplFactory<FFT2DImpl>, IFFT2D);
-
-// // TODO: The following code is a workaround for a bug with networks reshaping from Python.
-// // Replece it to nGraph nodes management with the release after 2020.1
-
-// class FFTShapeInferImpl : public IShapeInferImpl {
-//     StatusCode inferShapes(const std::vector<Blob::CPtr>& inBlobs,
-//                            const std::map<std::string, std::string>& params,
-//                            const std::map<std::string, Blob::Ptr>& blobs,
-//                            std::vector<SizeVector>& outShapes,
-//                            ResponseDesc* desc) noexcept override
-//     {
-//         outShapes.resize(inBlobs.size());
-//         for (size_t i = 0; i < inBlobs.size(); ++i)
-//             outShapes[i] = inBlobs[i]->getTensorDesc().getDims();
-//         return StatusCode::OK;
-//     }
-// };
-
-// class FFTShapeInferFactory : public IShapeInferExtension {
-//     virtual void SetLogCallback(IErrorListener& listener) noexcept {}
-//     virtual void GetVersion(const Version*& versionInfo) const noexcept {}
-//     virtual void Unload() noexcept {}
-//     virtual void Release() noexcept {}
-
-//     virtual StatusCode getShapeInferTypes(char**& types, unsigned int& size, ResponseDesc* resp) noexcept {
-//         size = 2;
-//         types = new char*[size];
-
-//         const char* fftType = "FFT2D";
-//         types[0] = new char[sizeof(fftType)];
-//         memcpy(types[0], fftType, sizeof(fftType));
-
-//         const char* ifftType = "IFFT2D";
-//         types[1] = new char[sizeof(ifftType)];
-//         memcpy(types[1], ifftType, sizeof(ifftType));
-
-//         return StatusCode::OK;
-//     }
-
-//     virtual StatusCode getShapeInferImpl(IShapeInferImpl::Ptr& impl, const char* type, ResponseDesc* resp) noexcept {
-//         impl = std::make_shared<FFTShapeInferImpl>();
-//         return StatusCode::OK;
-//     }
-// };
-
-// class FFT2DShapeInferFactory {
-// public:
-//     FFT2DShapeInferFactory() {
-
-//       // We need to do it to trigger custom shape inference registration.
-//       const std::string ir = R"V0G0N(
-//       <?xml version="1.0" ?>
-//       <net name="" version="10">
-//         <layers>
-//           <layer id="0" name="Placeholder_2" type="Parameter" version="opset1">
-//             <data element_type="f32" shape="1"/>
-//             <output>
-//               <port id="0" precision="FP32">
-//                 <dim>1</dim>
-//               </port>
-//             </output>
-//           </layer>
-//         </layers>
-//       </net>
-//       )V0G0N";
-
-//       InferenceEngine::Core ie;
-//       InferenceEngine::CNNNetwork net = ie.ReadNetwork(ir, Blob::CPtr());
-//       net.AddExtension(std::make_shared<FFTShapeInferFactory>());
-//     }
-// };
-
-// static FFT2DShapeInferFactory factory;
-
-// }  // namespace Cpu
-// }  // namespace Extensions
-// }  // namespace InferenceEngine
